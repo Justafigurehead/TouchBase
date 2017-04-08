c72ac06685d352feed76d74fb5b63f9d1744f4bf
Justafigurehead <strachanmichaela@hotmail.com>
2017-04-08 16:00:58 +0100

    Changed index.html to main.html.

--- controllers/journal.js
+++ controllers/journal.js
@@ -2,4 +2,16 @@ var express = require('express');
 var app = express();
 var journalRouter = express.Router();
 
-// var JournalQuery = require('../client/src/models/');
+var Entry = require('../client/src/models/entry');
+
+var JournalQuery = require('../client/src/db/journalQuery');
+var query = new JournalQuery();
+
+journalRouter.get('/', function(req, res){
+  query.all(function(results){
+    debugger;
+    res.sendFile(path.join(__dirname + '/../client/build/journal.html'));
+  });
+});
+
+module.exports = journalRouter;
