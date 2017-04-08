73a8ade70a4654d7ae67f4d4f7feea714d7fc441
3ll3n <ellengrafton01@gmail.com>
2017-04-08 13:05:35 +0100

    Added add function to journalQuery.js

---
+++ client/db/journalQuery.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient;
+
+var JournalQuery = function() {
+  this.url = 'mongodb://localhost:27017/travel_journal'; //connects into the mongodb
+};
+
+JournalQuery.prototype = {
+  all: function(onQueryFinished) {
+    MongoClient.connect(this.url, function(err, db) {
+      var collection = db.collection('entries');
+      collection.find().toArray(function(err, docs) {
+        onQueryFinished(docs);
+      });
+    });
+  }
+};
+
