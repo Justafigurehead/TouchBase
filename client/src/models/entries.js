ab95015cfee5103a0bac11b3dab93f851bfe0e0d
JodieGarden <jodie.garden@gmail.com>
2017-04-08 14:22:48 +0100

    Added entries.prototype with an makeRequest to database.

---
+++ client/src/models/entries.js
@@ -0,0 +1,26 @@
+var Entry = require('./entry');
+
+var Entries = function(){
+
+}
+
+Entries.prototype = {
+  makeRequest: function(url, callback){
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = callback;
+    request.send();
+  },
+  all: function(callback){
+    this.makeRequest("http://localhost:3000/journal", function(){
+      if(this.status !==200) return;
+      var jsonString = this.responseText;
+      var results = JSON.parse(jsonString);
+
+      var entries = Entries.prototype.populateEntries(results);
+        callback(entries);
+    });
+  }
+}
+
+module.exports = Entries;
