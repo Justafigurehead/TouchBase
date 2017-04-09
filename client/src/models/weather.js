f3219a1f86952eeb013299454d781dd004c67189
3ll3n <ellengrafton01@gmail.com>
2017-04-09 14:11:33 +0100

    Added makeRequest function to weather.js and created weather.js controller

---
+++ client/src/models/weather.js
@@ -0,0 +1,15 @@
+var Weather = function() {
+
+};
+
+Weather.prototype = {
+  makeRequest: function(url, callback) {
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onload = callback;
+    request.send();
+  }
+
+}
+
+module.exports = Weather;
