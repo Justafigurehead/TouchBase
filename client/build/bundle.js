f79e163a98ca6271846a3d9f57a6215b1a0d6c25
Justafigurehead <strachanmichaela@hotmail.com>
2017-04-10 14:35:57 +0100

    Editted UI to show pretty time with pm and current date as well as British time.

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -193,9 +193,9 @@ UI.prototype = {
     titleInput.setAttribute("name", "title");
     form.appendChild(titleInput);
 
-    var dateInput = document.createElement('input');
-    dateInput.setAttribute("name", "date");
-    form.appendChild(dateInput);
+    // var dateInput = document.createElement('input');
+    // dateInput.setAttribute("name", "date");
+    // form.appendChild(dateInput);
 
     var entryTextInput = document.createElement('input');
     entryTextInput.setAttribute("name", "entry_text");
@@ -206,12 +206,21 @@ UI.prototype = {
     button.innerText = 'Add Entry';
     form.appendChild(button);
 
+    var dateNow = function(){
+      var date = new Date();
+      var time = date.toLocaleTimeString();
+      var YMD = date.toLocaleDateString('en-GB');
+      var prettyDate = YMD + " " + time ;
+      console.log(prettyDate);
+      return prettyDate;
+    }
+
     form.onsubmit = function(e){
       e.preventDefault();
       console.log("Button Clicked")
       var newEntry = {
         title: e.target.title.value,
-        date: e.target.date.value,
+        date: dateNow(),
         entry_text: e.target.entry_text.value
       }
       
