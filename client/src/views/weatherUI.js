90b69e94a7f3f80ee1d78f367cb5fe2a1d33218f
JodieGarden <jodie.garden@gmail.com>
2017-04-10 14:05:34 +0100

    Created weatherRender function. Still to be completed.

--- client/src/views/weatherUI.js
+++ client/src/views/weatherUI.js
@@ -6,6 +6,31 @@ var WeatherUI = function(){
 
 WeatherUI.prototype = {
   weatherRender: function(weather){
+    var container = document.getElementById('weatherInfo');
+
+    var div = document.createElement('div');
+
+
+
+    var ul = document.createElement('ul');
+
+    var liName = document.createElement('li');
+    liName.innerHTML = "<p>" + weather.name + "</p>";
+
+    var liTemp = document.createElement('li');
+    liTemp.innerHTML = "<p>" + weather.main.temp + "</p>"
+
+    var liWeatherDescription = document.createElement('li');
+    liWeatherDescription.innerHTML = "<p>" + weather.weather.description + "</p>"
+
+    var imgIcon = document.createElement('img');
+    imgIcon.src = weather.weather.icon;
+
+
     
-  }
+
+
+
+
+  };
 }
