05991db7e8e391d10bc8dd8cff1da21dd38fb8a3
amd61 <arlenem.mcdaid@googlemail.com>
2017-04-11 10:30:59 +0100

    created populate Ul function

--- client/src/views/awayUI.js
+++ client/src/views/awayUI.js
@@ -17,9 +17,26 @@ dropdown.addEventListener("change", function () {
   var value = document.getElementById("away-dropdown").value;
   var country = new Away();
   country.selectedCountry(value, this.populateUl);
-  
-})
+
+});
+
+},
+
+populateUl: function(countryInfo){
+  var ul = document.createElement('ul');
+  var liName = document.createElement('li');
+  liName.innerText = countryInfo.name;
+  var liCap = document.createElement('li');
+  liName.innerText = countryInfo.capital;
+  var liCurr = document.createElement('li');
+  liCurr.innerText = countryInfo.currencies.name;
+  var liLang = document.createElement('li');
+  liLang.innerText = countryInfo.languages.name;  
+  ul.appendChild(liName);
+  ul.appendChild(liCap);
+  ul.appendChild(liCurr);
+  ul.appendChild(liLang);
+}
 }
 
-
-}
+module.exports = AwayUI;
