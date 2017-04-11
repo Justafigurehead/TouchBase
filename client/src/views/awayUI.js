78c8277e0e1402864605b8a760a9deb34a682bbf
amd61 <arlenem.mcdaid@googlemail.com>
2017-04-11 10:08:21 +0100

    render function completed

--- client/src/views/awayUI.js
+++ client/src/views/awayUI.js
@@ -1,6 +1,7 @@
 var Away = require('../models/away');
 
 var AwayUI = function(){
+
 }
 
 AwayUI.prototype = {
@@ -8,10 +9,16 @@ render: function(away){
   var dropdown = document.getElementById('away-dropdown');
   for( var countries of away){
     var option = document.createElement('option');
+    option.innerText = name;
+    dropdown.appendChild(option);
 
-   
   }
-
+dropdown.addEventListener("change", function () {
+  var value = document.getElementById("away-dropdown").value;
+  var country = new Away();
+  country.selectedCountry(value, this.populateUl);
+  
+})
 }
 
 
