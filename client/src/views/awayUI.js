f1242986cf77f10a20da6af81c61f46a259813ae
amd61 <arlenem.mcdaid@googlemail.com>
2017-04-12 09:04:55 +0100

    various fixes awayUI.js - country info API

--- client/src/views/awayUI.js
+++ client/src/views/awayUI.js
@@ -1,48 +1,65 @@
 var Away = require('../models/away');
+var util = require('util')
 
-var AwayUI = function(){
-
+var AwayUI = function() {
 };
 
 AwayUI.prototype = {
-  render: function(away){
-    var dropdown = document.getElementById('away-dropdown');
-    for( var country of away){
-      var option = document.createElement('option');
-      option.innerText = country.name;
-      dropdown.appendChild(option);
 
-    };
-    dropdown.addEventListener("change", function () {
-      var value = document.getElementById("away-dropdown").value;
-      var country = new Away();
-      country.all(value, this.populateUl);
-    });
+    countrySelected: function() {
+        console.log("Country picked");
+        var value = document.getElementById("away-dropdown").value;
+        console.log("country: " + value);
+        var country = new Away();
+        country.getCountry(value, this.populateUl);
 
-  },
+    },
 
+    populateUl: function(countryInfo) {
+    },
 
-  populateUl: function(countryInfo){v
-    var div = document.getElementById('away');
-    var ul = document.createElement('ul');
-    var liName = document.createElement('li');
-    liName.innerText = countryInfo.name;
-    var liCap = document.createElement('li');
-    liName.innerText = countryInfo.capital;
-    var liCurr = document.createElement('li');
-    liCurr.innerText = countryInfo.currencies.name;
-    var liLang = document.createElement('li');
-    liLang.innerText = countryInfo.languages.name;  
-    ul.appendChild(liName);
-    ul.appendChild(liCap);
-    ul.appendChild(liCurr);
-    ul.appendChild(liLang);
-    div.appendChild(ul);
-  },
-  showCountries:function(){   
-    var away = new Away();
-    away.all(this.render);
-  }
+    render: function(away) {
+        var dropdown = document.getElementById('away-dropdown');
+        for (var country of away) {
+            var option = document.createElement('option');
+            option.innerText = country.name;
+            dropdown.appendChild(option);
+
+        };
+        dropdown.addEventListener("change",
+            function() {
+                console.log("Country picked");
+                var value = document.getElementById("away-dropdown").value;
+                console.log("country: " + value);
+                var country = new Away();
+               
+                country.getCountry(value, function(countryInfo) {
+                    console.log("country info: " + countryInfo);
+                    var results = JSON.parse(countryInfo);
+                    console.log("results: " + util.inspect(results));
+                    var div = document.getElementById('away');
+                    var ul = document.createElement('ul');
+                    var liName = document.createElement('li');
+                    liName.innerText = results[0].name;
+                    var liCap = document.createElement('li');
+                    liCap.innerText = results[0].capital;
+                    var liCurr = document.createElement('li');
+                    liCurr.innerText = results[0].currencies[0].name;
+                    var liLang = document.createElement('li');
+                    liLang.innerText = results[0].languages[0].name;
+                    ul.appendChild(liName);
+                    ul.appendChild(liCap);
+                    ul.appendChild(liCurr);
+                    ul.appendChild(liLang);
+                    div.appendChild(ul);
+                });
+            })
+    },
+
+    showCountries: function() {
+        var away = new Away();
+        away.all(this.render);
+    }
 }
 
 module.exports = AwayUI;
